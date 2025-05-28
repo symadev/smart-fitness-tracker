e8b4829fc5c23dacefb6412ab9b019ff6b217769
syma sultana <symasultana2002@gmail.com>
2025-05-28 11:55:49 +0600

    edited the dashboard

--- src/Components/Pages/DashBoard.jsx
+++ src/Components/Pages/DashBoard.jsx
@@ -1,22 +1,24 @@
 // Dashboard.jsx
 import { NavLink, Outlet } from "react-router-dom";
 import { FaHome, FaDumbbell, FaRobot, FaCog } from "react-icons/fa";
+import logo from "../../assets/images/gym logo.png"
 
 const Dashboard = () => {
   return (
     <div className="flex">
       {/* Sidebar */}
-      <aside className="w-64 min-h-screen bg-[#0D1124] text-white flex flex-col">
+      <aside className="w-64 min-h-screen bg-[#0f1f60] text-white flex flex-col">
         {/* Logo */}
-        <div className="flex items-center gap-2 px-6 py-6">
-          <div className="bg-gradient-to-br from-blue-400 to-cyan-500 w-10 h-10 rounded-full flex items-center justify-center text-xl font-bold">
-            L
+        <div className="grid items-center gap-2 ml-24  py-6">
+          <div className=" w-16 h-16 rounded-full  items-center justify-center text-xl font-bold">
+           <img src={logo} alt="" />
+              <span className="text-xl font-bold">SmartFit</span>
           </div>
-          <span className="border px-2 py-1 rounded text-sm">LOGO</span>
+         
         </div>
 
         {/* Section Title */}
-        <h2 className="text-xs text-gray-400 px-6 mb-4">MENU</h2>
+        <h2 className="text-xs  text-gray-400 px-6 mb-4">MENU</h2>
 
         {/* Navigation Links */}
         <ul className="flex flex-col gap-2 px-4 text-sm font-medium">
@@ -35,12 +37,10 @@ const Dashboard = () => {
         </ul>
 
         {/* Footer */}
-        <div className="mt-auto px-6 text-xs text-gray-400 py-6">
-          <h3 className="mb-1">LOREM IPSUM</h3>
-          <p className="mb-4">Lorem ipsum dolor sit amet, consectetur adipiscing</p>
-          <button className="bg-[#1E213A] hover:bg-[#2d3160] text-white text-sm px-4 py-2 rounded shadow-md">
-            LOREM
-          </button>
+        <div className="mt-auto px-6 text-xs text-white py-6">
+          <h3 className="mb-1">SmartFit</h3>
+          <p className="mb-4">Be stronger than your excuses. Welcome to SmartFit</p>
+         
         </div>
       </aside>
 
